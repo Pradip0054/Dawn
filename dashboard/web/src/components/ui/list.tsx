c9388e481b63d836d8fbbe70598dbcf9157d058c
BiltuDas1 <billionto@gmail.com>
2026-01-30 13:37:43 +0530

    Changed logic to show Local TimeZone

--- dashboard/web/src/components/ui/list.tsx
+++ dashboard/web/src/components/ui/list.tsx
@@ -12,6 +12,35 @@ function List() {
     }, []
   )
 
+  function getTimeZone(date: Date) {
+    const offset = date.getTimezoneOffset();
+    const hour = Math.trunc(-offset / 60);
+    const minutes = -offset % 60;
+    if (offset < 0) {
+      return `+${hour}:${minutes}`;
+    }
+    if (offset > 0) {
+      return `-${hour}:${minutes}`;
+    }
+    return "";
+  }
+
+  function toLocalDateTime(date: string) {
+    const dateTime = new Date(date.replace(" ", "T") + "Z");
+    const options: Intl.DateTimeFormatOptions = {
+      hour: 'numeric',
+      minute: '2-digit',
+      hour12: true
+    };
+    const dateTimeStr = `${dateTime.toLocaleDateString()} ${dateTime.toLocaleTimeString(undefined, options).toLocaleUpperCase()} (GMT${getTimeZone(dateTime)})`;
+
+    return (
+      <>
+        {dateTimeStr}
+      </>
+    )
+  }
+
   const generateTable = () => {
     if (result === null) {
       return;
@@ -21,7 +50,7 @@ function List() {
       <tr>
         <td><a href={row.url} target="_blank">{row.job_role}</a></td>
         <td>{row.company}</td>
-        <td>{row.added_at}</td>
+        <td>{toLocalDateTime(row.added_at)}</td>
       </tr>
     ))
   }
